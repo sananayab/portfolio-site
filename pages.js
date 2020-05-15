01cd3828d19f79ec0b9dc8307e8af2a3d2d7e24a
Jeffery John <53410876+JefferyJohn@users.noreply.github.com>
2020-05-15 10:03:17 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/JefferyJohn/jefferyjohn.github.io.git";
+const repoURL = "https://github.com/JefferyJohn/JefferyJohn.github.io.git";
 
 ghpages.publish(
   pathname,
