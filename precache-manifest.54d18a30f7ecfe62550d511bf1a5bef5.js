4cdaa9367fb6188acc4a8cb98a5f3b69ce9e29d8
Jeffery John <jefferyjohn2000@gmail.com>
2020-07-06 12:10:11 -0400

    Updates

--- precache-manifest.54d18a30f7ecfe62550d511bf1a5bef5.js
+++ precache-manifest.54d18a30f7ecfe62550d511bf1a5bef5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a0bde984ff464c752e3a91b25151f995",
+    "revision": "c8c249c4c386aa35c4ddb625ba978a6e",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "f39256ed4b98b3458ccc",
+    "revision": "30e93811a91258e8fb10",
     "url": "/static/css/main.bca54414.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e80cd1fb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f39256ed4b98b3458ccc",
-    "url": "/static/js/main.38242958.chunk.js"
+    "revision": "30e93811a91258e8fb10",
+    "url": "/static/js/main.9a7e89f1.chunk.js"
   },
   {
     "revision": "b7af62f302c459d5ed47",
